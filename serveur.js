2bd018a49429e10967c0751e57dd6ff93efbacfd
abdelSaouli <saouli.abdelwahhab@yahoo.fr>
2018-11-11 16:46:41 +0100

    ajout body-parser

--- serveur.js
+++ serveur.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const session = require('express-session');
 const app = express();
+const bodyparser = reqyure('body-parser');
+
+
+app.use(bodyparser.urlencoded({extended: true}));
 
 app.use(session({secret: 'test'}));
 
